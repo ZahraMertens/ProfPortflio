a833f3b030f001798b6c85333c662afaadb8b729
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-09-18 13:59:14 +1000

    Update portfolio

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+var win = $(window);
+var allMods = $(".module");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
